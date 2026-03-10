14597c3c221ae46d2160236e741eac77ca82d4eb
VicenteAlarconC <vicente.i.alarcon@gmail.com>
2026-03-10 19:11:35 -0300

    feat: add ApiOkResponse decorator to populateDB method for improved API documentation

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { SeedService } from './seed.service';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
+  @ApiOkResponse({
+    description: 'Database populated successfully',
+  })
   @Get()
   populateDB() {
     return this.seedService.populateDB();
